71c9931541b43cb02b3ab601d56476a3d7c7f188
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-01-29 11:27:02 -0500

    initialize main-view component

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,11 @@
+export const MainView = () => {
+  return (
+    <div>
+      <div>Eloquent JavaScript</div>
+      <div>Mastering Javascript Functional Programming</div>
+      <div>Javascript: The Good Parts</div>
+      <div>Javascript: The Definitive Guide</div>
+      <div>The Road to React</div>
+    </div>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { MainView } from "./components/main-view/main-view";
 import "./index.scss";
 
 const MyFlixApplication = () => {
   return (
-    <div className='my-flix'>
-      <div>Good Evening</div>
-    </div>
+    <MainView />
   );
 };
 

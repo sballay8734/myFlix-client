f4484f05e0c77355575f4e5f56bcc91dc60ee216
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-02-01 14:56:42 -0500

    added more details to book

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,11 +4,31 @@ import { BookCard } from "./book-card/book-card";
 export const MainView = () => {
 
   const [books, setBooks] = useState([
-    { id: 1, title: "Eloquent Javascript" },
-    { id: 2, title: "Mastering Javascript Functional Programming" },
-    { id: 3, title: "Javascript: The Good Parts" },
-    { id: 4, title: "Javascript: The Definitive Guide" },
-    { id: 5, title: "The Road to React" }
+    { id: 1,
+      title: "Eloquent Javascript",
+      image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+      author: "Marijn Haverbeke"
+    },
+    { id: 2,
+      title: "Mastering Javascript Functional Programming",
+      image: "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      author: "Federico Kereki"
+    },
+    { id: 3,
+      title: "Javascript: The Good Parts",
+      image: "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
+      author: "Douglas Crockford"
+    },
+    { id: 4,
+      title: "Javascript: The Definitive Guide",
+      image: "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      "David Flanagan"
+    },
+    { id: 5,
+      title: "The Road to React",
+      image: "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
+      author: "Robin Wieruch"
+    }
   ]); // first item (books) is current state, second item (setBooks) is function that updates current state
 
   if (books.length === 0) {
